df398f151ae5f8bf153cc6086518023575e86e0a
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-08 21:53:09 -0800

    Update programs.nix

--- modules/programs.nix
+++ modules/programs.nix
@@ -24,6 +24,7 @@
     fragments          # Torrenting client
     bottles            # Wine manager 
     ghex               # Hex editor
+    impression         # Bootable drive maker
     
   ];
 
