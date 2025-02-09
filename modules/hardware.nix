4a401b7f6488be039ba67264b0a2995868e93ecb
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-09 00:52:16 -0800

    Update hardware.nix

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,14 +1,33 @@
 { ... }: {
 
+  # Set platform type
   nixpkgs.hostPlatform = "x86_64-linux";
-  
+
+  # Set time zone
   time.timeZone = "America/Los_Angeles";
-  
+
+  # Enable pipewire for audio
+  hardware.pulseaudio.enable = false;
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable CUPS for printing
+  services.printing.enable = true;
+
+  # Configure network
   networking = {
     hostName = "laptop";
     useDHCP = true;
   };
-  
+
+  # Configure bootloader
   boot = {
     loader.grub = {
       enable = true;
@@ -25,6 +44,7 @@
     ];
   };
 
+  # Configure file system
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/9a80ebfd-54d6-410d-a8c3-25fc3cc7c1dd";
     fsType = "ext4";
