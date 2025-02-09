f905a74f3a4a217f7f65924579dc61fc51a12146
tob4n <109690594+tob4n@users.noreply.github.com>
2025-02-08 21:16:33 -0800

    Create gnome.nix

---
+++ modules/gnome.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }: {
+  
+  # Enable GNOME
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  # Enable XDG desktop portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
+
+  # Disable GNOME core apps and setup
+  services.gnome = {
+    core-utilities.enable = false;
+    gnome-initial-setup.enable = false;
+  };
+
+  # Disable remaining GNOME apps
+  environment.gnome.excludePackages = [
+    pkgs.gnome-tour
+    pkgs.gnome-shell-extensions
+  ];
+
+}
+
+}
